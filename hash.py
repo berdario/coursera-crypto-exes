74713c42c9546b9a95ffacd48417791b9a3c2453
Dario Bertini <berdario@gmail.com>
2012-03-31 22:49:27 +0200

    Added comments and small cleanup

--- hash.py
+++ hash.py
@@ -1,7 +1,5 @@
 import random
-#from Crypto.Hash import SHA256
 from hashlib import sha256
-from collections import defaultdict
 from time import time
 from string import printable
 from itertools import product, chain, count, islice, groupby
@@ -33,19 +31,24 @@ def get_rand_block():
 		yield next(blocks)
 
 def get_proc_block():
-	blocks = chain(*[product(*([alphabet]*i)) for i in range(60)])
+	blocks = chain(*[product(*([alphabet]*i)) for i in count()])
 	while True:
 		yield "".join(next(blocks))
 
 def results(array, ctrlq, resultq, offset, step, slice_size=100000):
+	# the bigger the slice_size, the easier that some collisions may slip through
+	# but, set it too small, and the IO will slow everything down, 100k should be a good compromise
 	db = wrapping_bloomify(baredb, "hash", array)
 	t = time()
 	elements = {}
 	slices = (v for k,v in islice(groupby(enumerate(get_proc_block()), lambda t: t[0]/slice_size), offset, None, step))
+	# this will return the strings, paired with an increasing index, and group them by slice size
+	# e.g. with slice_size=100000, offset=1 (process "id"), and step=2 (2 processes)
+	# it'll return the elements from 100000 to 199999 then from 300000 to 399999 and so on
 	
 	for s in slices:
 		for i, value in s:
-			key = get_lsbs_str(sha256(value).digest(), 41).encode("hex")
+			key = get_lsbs_str(sha256(value).digest()).encode("hex")
 			el = elements.get(key)
 			el = el or db.find_one({"hash": key})
 			if el:
