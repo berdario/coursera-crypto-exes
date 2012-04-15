bdddd90d47be2623302a3b2b8ce45d5246b8068a
Dario Bertini <berdario@gmail.com>
2012-04-15 15:58:23 +0200

    Converted some sequence expressions to lists Removed some useless Array to Seq conversions (Now it should be about twice as fast)

--- hash.fs
+++ hash.fs
@@ -1,12 +1,12 @@
 open System.Security.Cryptography
 open System.Collections.Generic
 
-let printables = seq{for c in Seq.map char (seq{0..127}) do if not (System.Char.IsControl(c)) then yield c}
+let printables = [for c in List.map char ([0..127]) do if not (System.Char.IsControl(c)) then yield c]
 
 let rec product xss =
     match xss with
-    | [] -> Seq.singleton Seq.empty
-    | (xs::yss) -> seq{for p in (product yss) do for x in xs do yield Seq.append (Seq.singleton x) p }
+    | [] -> Seq.singleton []
+    | (xs::yss) -> seq{for p in (product yss) do for x in xs do yield x::p }
 
 let words = Seq.initInfinite (fun i -> List.replicate i printables |> product) |> Seq.concat |> Seq.map (fun x -> System.String.Join("", x))
 
@@ -18,24 +18,25 @@ let pick_collision (i, (bytes, hash)) =
     else
         do
             ignore (results.Add(hash, bytes))
-            if i%100000 = 0 then do
-                printfn "%A" i
+            if i%1000000 = 0 && i>0 then do
+                //printfn "%A" i
+                System.Environment.Exit 0
         None
 
 let flag x = (log (float x) / log 2.0 + 1.0 |> int |> pown 2 )-1
 
-let padded_byte x wbyte : seq<byte> =
+let padded_byte x wbyte =
     if x>0 then
-        byte (flag x) &&& wbyte |> Seq.singleton
+        [|byte (flag x) &&& wbyte|]
     else
-        Seq.empty
+        [||]
 
 let last_bits n (bytes: byte[]) =
     let l = bytes.Length
     let n_bytes = n/8
     let rem = n%8
     let wbyte = padded_byte rem bytes.[l-n_bytes-1]
-    bytes |> Seq.skip (l - n_bytes) |> Seq.append wbyte |> Seq.toList
+    bytes.[l-n_bytes..] |> Array.append wbyte |> Array.toList
 
 let find_collisions =
     let sha256 = new SHA256Managed()
